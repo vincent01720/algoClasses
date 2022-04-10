6558fd456db75b6813636ae9ebab92275e541101
Vincent Yim <vincent.yim@adcolespace.com>
2022-04-10 11:46:54 -0400

    class

---
+++ matlab/OOP_radar/blip.m
@@ -0,0 +1,12 @@
+classdef blip
+    %UNTITLED2 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        AoA
+        range
+        signal
+    end
+    
+end
+
